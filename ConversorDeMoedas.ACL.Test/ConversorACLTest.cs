3de9bf99d12314ec9c73a610f1172d5a779ebe75
Gabriel Rodrigues <grfgabriel.ti@gmail.com>
2018-07-18 13:25:15 -0300

    Subindo testes de ACL com ex de mock do redis.

--- ConversorDeMoedas.ACL.Test/ConversorACLTest.cs
+++ ConversorDeMoedas.ACL.Test/ConversorACLTest.cs
@@ -10,18 +10,25 @@ using Microsoft.Extensions.Caching.Distributed;
 using ConversorDeMoedas.Infrastructure.Interface.Factory;
 using Moq;
 using ConversorDeMoedas.Domain;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace ConversorDeMoedas.ACL.Test
 {
     public class ConversorACLTest
     {
-    
+
         [Fact]
         public void TestQueDeveRetornarListaComAsMoedasSemValorSomenteSiglaENome()
         {
+            List<IMoeda> mcklist = new List<IMoeda>();
             byte[] resultmockNull = null;
-             Mock<IDistributedCache> mckcache = new Mock<IDistributedCache>();      
-            mckcache.Setup(x =>x.Get("GetMoedasList")).Returns(resultmockNull);
+            Mock<IDistributedCache> mckcache = new Mock<IDistributedCache>();
+            mckcache.Setup(x => x.Get("GetMoedasList")).Returns(resultmockNull);
+
+            DistributedCacheEntryOptions distributedCacheEntryOptions = new DistributedCacheEntryOptions();
+            distributedCacheEntryOptions.SetAbsoluteExpiration(TimeSpan.FromMinutes(1));
+            mckcache.Setup(x => x.Set("GetMoedasList", Serialize(mcklist), distributedCacheEntryOptions));
             IConversorACL conversorACL = new ConversorACL(new MoedaFactory(), new RedisConnectorHelperFactory(mckcache.Object));
             List<IMoeda> result = conversorACL.GetMoedas();
             Assert.True(result.Count > 0);
@@ -30,16 +37,40 @@ namespace ConversorDeMoedas.ACL.Test
         [Fact]
         public void TestQueRetornaACotacaoDaMoedaDesejadaComBaseNoDolar()
         {
-           
-            // String SiglaMoedaDesejada = "USD";
 
-            // IConversorACL conversorACL = new ConversorACL(new MoedaFactory(), new RedisConnectorHelperFactory());
-            // IMoeda DolarResult = conversorACL.GetCotacaoComBaseNoDolar(SiglaMoedaDesejada);
-            // Assert.True(DolarResult.Valor.Equals(1));
+            String SiglaMoedaDesejada = "USD";
 
-            // String OutraMoedaQueNaoTemValorDoDolar = "BRL";
-            // IMoeda RealResult = conversorACL.GetCotacaoComBaseNoDolar(OutraMoedaQueNaoTemValorDoDolar);
-            // Assert.False(RealResult.Valor.Equals(DolarResult.Valor));
+            IMoeda mckMoeda = new Moeda("USD", 1);
+            byte[] resultmockNull = null;
+            Mock<IDistributedCache> mckcache = new Mock<IDistributedCache>();
+            mckcache.Setup(x => x.Get("GetCotacaoComBaseNoDolarUSD")).Returns(resultmockNull);
+            DistributedCacheEntryOptions distributedCacheEntryOptions = new DistributedCacheEntryOptions();
+            distributedCacheEntryOptions.SetAbsoluteExpiration(TimeSpan.FromMinutes(1));
+            mckcache.Setup(x => x.Set("GetCotacaoComBaseNoDolarUSD", Serialize(mckMoeda), distributedCacheEntryOptions));
+            IConversorACL conversorACL = new ConversorACL(new MoedaFactory(), new RedisConnectorHelperFactory(mckcache.Object));
+            IMoeda DolarResult = conversorACL.GetCotacaoComBaseNoDolar(SiglaMoedaDesejada);
+            Assert.True(DolarResult.Valor.Equals(1));
+
+            String OutraMoedaQueNaoTemValorDoDolar = "BRL";
+            mckcache.Setup(x => x.Get("GetCotacaoComBaseNoDolarBRL")).Returns(resultmockNull);
+            mckcache.Setup(x => x.Set("GetCotacaoComBaseNoDolarBRL", Serialize(mckMoeda), distributedCacheEntryOptions));
+
+            IMoeda RealResult = conversorACL.GetCotacaoComBaseNoDolar(OutraMoedaQueNaoTemValorDoDolar);
+            Assert.False(RealResult.Valor.Equals(DolarResult.Valor));
+        }
+        private static byte[] Serialize(object obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+            BinaryFormatter objBinaryFormatter = new BinaryFormatter();
+            using (MemoryStream objMemoryStream = new MemoryStream())
+            {
+                objBinaryFormatter.Serialize(objMemoryStream, obj);
+                byte[] objDataAsByte = objMemoryStream.ToArray();
+                return objDataAsByte;
+            }
         }
     }
 }
